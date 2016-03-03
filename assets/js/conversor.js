6db19eb907cfc91bb1dc63b50eaaca2e1c0673c7
marrero- <alexmd54@gmail.com>
2016-03-03 19:43:21 +0000

    Se ha arreglado el error en el uso de la expresión regular para validar. :ok_hand: .  Se ha añadido un icono de Github en el html para enlazar el repo.  Además se han añadido sentencias de control para ser más específico con el usuario en caso de error. :+1:  Este commit cierra los issues #1 #2 #4

--- assets/js/conversor.js
+++ assets/js/conversor.js
@@ -1,6 +1,5 @@
 (function (exports) {
 		"use strict";
-
 		function Medida(valor, tipo) {
 			this.value = valor;
 			this.type = tipo;
@@ -12,6 +11,7 @@
 
 		function Celsius(valor) {
 			Temperatura.call(this, valor, "c");
+
 			this.toFarenheit = function () {
 				return ((valor * 9 / 5) + 32);
 			};
@@ -37,7 +37,6 @@
 			Temperatura.call(this, valor, "k");
 
 			this.toCelsius = function () {
-
 				return (valor - 237.15);
 			};
 
@@ -45,72 +44,65 @@
 				return (((valor * 9) / 5) - 459.67);
 			};
 		}
-		Temperatura.prototype = new Medida();
-		Temperatura.prototype.constructor = Temperatura;
-		Celsius.prototype = new Temperatura();
-		Celsius.prototype.constructor = Celsius;
-		Farenheit.prototype = new Temperatura();
-		Farenheit.prototype.constructor = Farenheit;
-		Kelvin.prototype = new Temperatura();
-		Kelvin.prototype.constructor = Kelvin;
+Temperatura.prototype = new Medida();
+Temperatura.prototype.constructor = Temperatura;
+Celsius.prototype = new Temperatura();
+Celsius.prototype.constructor = Celsius;
+Farenheit.prototype = new Temperatura();
+Farenheit.prototype.constructor = Farenheit;
+Kelvin.prototype = new Temperatura();
+Kelvin.prototype.constructor = Kelvin;
+exports.Temperatura = Temperatura;
+exports.Celsius = Celsius;
+exports.Farenheit = Farenheit;
 
-		exports.Temperatura = Temperatura;
-		exports.Celsius = Celsius;
-		exports.Farenheit = Farenheit;
-
-		exports.convertir = function(){
-    var valor     = document.getElementById('convert').value,
-        elemento  = document.getElementById('converted'),
-        /* Extienda la RegeExp a la especificación. use una XRegExp */
-        regexp    = /^\s*([-+]?\d+(?:\.\d+)?(?:e[+-]?\d+)?)\s*([fkc])\s*(?:to)?\s*([fkc])$/i,
-
-        expresion = XRegExp('(?<num>    ^[ ]*[-+]?[0-9](.[0-9]+)?[ ]*((e[+-]?[ ]*[0-9]+)?)[ ]*)\n\
-                           (?<temp1>    [ ]*([fkcFKC]))\n\
-                           (?<to>       [ ]*(?:to)?[ ]*)\n\
-                           (?<temp2>    [fkcFKC])[ ]*$'),
-
-       valor = valor.match(regexp);
-    ///    valor = XRegExp.exec(valor, expresion);
-
-   if (valor) {
-   //if (match) {
-      var numero = valor[1],
-          tipo   = valor[2].toLowerCase(),
-          tipo2  = valor[3].toLowerCase();
-
-      numero = parseFloat(numero);
-      console.log("Valor: " + numero + ", Tipo: " + tipo);
-
-      switch (tipo) {
-        case 'c':
-          var celsius = new Celsius(numero);
-          if (tipo2 == 'f')
-          elemento.innerHTML = celsius.toFarenheit().toFixed(2) + " Farenheit.";
-          if (tipo2 == 'k')
-          elemento.innerHTML = celsius.toKelvin().toFixed(2) + " Kelvin.";
-
-          break;
-        case 'f':
-          var farenheit = new Farenheit(numero);
-          if (tipo2 == 'c')
-          elemento.innerHTML = farenheit.toCelsius().toFixed(2) + " Celsius";
-          if (tipo2 == 'k')
-          elemento.innerHTML = farenheit.toKelvin().toFixed(2) + " Kelvin";
-          break;
-        case 'k':
-          var kelvin = new Kelvin(numero);
-          if (tipo2 == 'c')
-          elemento.innerHTML = kelvin.toCelsius().toFixed(2) + " Celsius";
-          if (tipo2 == 'f')
-          elemento.innerHTML = kelvin.toFarenheit().toFixed(2) + " Farenheit";
-          break;
-
-        default:
-          elemento.innerHTML = "ERROR! Intenta algo como '-4.2C' ";
-      }
+exports.convertir = function(){
+	var valor     = document.getElementById('convert').value;
+  var  elemento  = document.getElementById('converted');
+	var expresion = XRegExp('^(?<value>[+-]?\\d+(\\.\\d+)?([e][+-]?\\d+)?[ ]*) # valor \n\
+											 (?<type1>[cfk])   #type for number   \n\
+											 (?<to>[ ]+(?:to[ ]+)?)  #optional to string \n\
+											 (?<type2>[cfk])[ ]*$    #type to convert', 'xi');
+  var match = XRegExp.exec(valor,expresion);
+  if (match) {
+  	var numero = match.value,
+        tipo   = match.type1.toLowerCase(),
+        tipo2  = match.type2.toLowerCase();
+    numero = parseFloat(numero);
+    console.log("Valor: " + numero + ", Tipo: " + tipo + " convertir a : " + tipo2);
+    switch (tipo) {
+      case 'c':
+        var celsius = new Celsius(numero);
+        if (tipo2 == 'f')
+        	elemento.innerHTML = celsius.toFarenheit().toFixed(2) + " Farenheit.";
+        else if (tipo2 == 'k')
+        			elemento.innerHTML = celsius.toKelvin().toFixed(2) + " Kelvin.";
+							else
+								elemento.innerHTML = "No se conoce la medida para convertir: " + tipo2;
+        break;
+      case 'f':
+        var farenheit = new Farenheit(numero);
+        if (tipo2 == 'c')
+        	elemento.innerHTML = farenheit.toCelsius().toFixed(2) + " Celsius";
+        else if (tipo2 == 'k')
+        			elemento.innerHTML = farenheit.toKelvin().toFixed(2) + " Kelvin";
+							else
+								elemento.innerHTML = "No se conoce la medida para convertir: " + tipo2;
+        break;
+      case 'k':
+        var kelvin = new Kelvin(numero);
+        if (tipo2 == 'c')
+        	elemento.innerHTML = kelvin.toCelsius().toFixed(2) + " Celsius";
+        else if (tipo2 == 'f')
+        		elemento.innerHTML = kelvin.toFarenheit().toFixed(2) + " Farenheit";
+						else
+							elemento.innerHTML = "No se conoce la medida para convertir: " + tipo2;
+        break;
+      default:
+        elemento.innerHTML = "ERROR! Intenta algo como '-4.2C to K' ";
     }
-    else
-      elemento.innerHTML = "";
   }
-
+    else
+      elemento.innerHTML = "Introduzca algo como 32.5K to C";
+  }
 })(this);
